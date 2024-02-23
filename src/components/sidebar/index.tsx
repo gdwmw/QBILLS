7a8ec9fcd26e340e8abdb0b45f432aa4562da11c
gdwmw <gededewowahyumw@gmail.com>
2024-02-23 09:57:46 +0700

    style - Sidebar | Component

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -18,7 +18,7 @@ export const Sidebar: FC = (): ReactElement => {
   const pathname = usePathname();
 
   return (
-    <aside className="fixed left-0 top-0 flex h-screen w-[300px] flex-col items-center gap-10 bg-N1.1 px-5 py-10">
+    <aside className="fixed left-0 top-0 z-10 hidden h-screen w-[300px] flex-col items-center gap-10 bg-N1.1 px-5 py-10 lg:flex">
       <section className="flex h-fit w-fit items-center justify-center gap-2">
         <Image src={logoQbills1} alt="Qbills" width={40} quality={30} priority />
         <div>
