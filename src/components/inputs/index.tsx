002dc154c063293b62a78caf2630b495017b02d0
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-03-19 04:58:39 +0700

    comp - Text Area

--- src/components/inputs/index.tsx
+++ src/components/inputs/index.tsx
@@ -1,1 +1,3 @@
 export * from "./input";
+export * from "./select";
+export * from "./text-area";
---
+++ src/components/inputs/text-area/index.tsx
@@ -0,0 +1,48 @@
+import { cn } from "@/libs";
+import { cva, type VariantProps } from "class-variance-authority";
+import { FC, ReactElement, TextareaHTMLAttributes, forwardRef } from "react";
+
+const TextAreaFieldsetCVA = cva("group w-full overflow-hidden rounded-md border-2 px-1 pb-2", {
+  variants: {
+    variant: {
+      default: "border-N2 focus-within:border-P4",
+      error: "border-E4",
+      disabled: "border-N3 bg-N2",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+  },
+});
+
+const TextAreaLegendCVA = cva("ml-3 select-none px-1 text-xs whitespace-nowrap font-semibold", {
+  variants: {
+    variant: {
+      default: "text-N3 group-focus-within:text-P5",
+      error: "text-E4",
+      disabled: "text-N4",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+  },
+});
+
+interface I extends TextareaHTMLAttributes<HTMLTextAreaElement>, VariantProps<typeof TextAreaFieldsetCVA> {
+  label?: string;
+  errorMassage?: string;
+}
+
+export const TextArea: FC<I> = forwardRef<HTMLTextAreaElement, I>(({ label, errorMassage, variant, className, ...props }, ref): ReactElement => {
+  return (
+    <section className="space-y-1">
+      <fieldset className={cn(TextAreaFieldsetCVA({ variant }))}>
+        <legend className={cn(TextAreaLegendCVA({ variant }))}>{label}</legend>
+        <textarea ref={ref} className={`w-full rounded-sm bg-transparent px-1 outline-none disabled:cursor-not-allowed ${className}`} {...props} />
+      </fieldset>
+      {errorMassage && <span className="ml-1 text-xs text-E4">{errorMassage}</span>}
+    </section>
+  );
+});
+
+TextArea.displayName = "TextArea";
