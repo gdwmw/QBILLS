69b8373c6ca9a2e4a67507f97b3339ffd2ef69ca
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-04-29 14:23:39 +0700

    feat - Smooth Scroll

--- src/modules/landing-page/header/index.tsx
+++ src/modules/landing-page/header/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { ButtonCVA } from "@/components";
 import logoQbills1 from "@/public/assets/images/logos/white/logo-2.png";
 import logoQbills2 from "@/public/assets/images/logos/white/logo-4.png";
@@ -8,6 +10,11 @@ import { FC, ReactElement } from "react";
 import { MobileNavbar } from "./mobile-navbar";
 
 export const Header: FC = (): ReactElement => {
+  function handleScroll(e: any, id: string) {
+    e.preventDefault();
+    document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
+  }
+
   return (
     <header>
       <nav className="absolute left-0 top-0 w-full px-10 py-5">
@@ -21,20 +28,50 @@ export const Header: FC = (): ReactElement => {
           </section>
 
           <section className="hidden items-center gap-24 lg:flex">
-            <Link href={"#Home"} className={ButtonCVA({ ghost: "white", className: "font-semibold" })}>
+            <Link
+              href={"#Home"}
+              className={ButtonCVA({ ghost: "white", className: "font-semibold" })}
+              onClick={(event) => {
+                handleScroll(event, "Home");
+              }}
+            >
               Home
             </Link>
-            <Link href={"#About-Us"} className={ButtonCVA({ ghost: "white", className: "whitespace-nowrap font-semibold" })}>
+            <Link
+              href={"#About-Us"}
+              className={ButtonCVA({ ghost: "white", className: "whitespace-nowrap font-semibold" })}
+              onClick={(event) => {
+                handleScroll(event, "About-Us");
+              }}
+            >
               About Us
             </Link>
-            <Link href={"#Our-Feature"} className={ButtonCVA({ ghost: "white", className: "font-semibold" })}>
+            <Link
+              href={"#Our-Feature"}
+              className={ButtonCVA({ ghost: "white", className: "font-semibold" })}
+              onClick={(event) => {
+                handleScroll(event, "Our-Feature");
+              }}
+            >
               Features
             </Link>
-            <Link href={"#FAQ"} className={ButtonCVA({ ghost: "white", className: "font-semibold" })}>
+            <Link
+              href={"#FAQ"}
+              className={ButtonCVA({ ghost: "white", className: "font-semibold" })}
+              onClick={(event) => {
+                handleScroll(event, "FAQ");
+              }}
+            >
               FAQ
             </Link>
             <div className="w-36">
-              <Link href={"#Get-App"} className={ButtonCVA({ solid: "white", size: "sm", widthFull: true, className: "font-semibold text-P4" })}>
+              <Link
+                href={"#Get-App"}
+                className={ButtonCVA({ solid: "white", size: "sm", widthFull: true, className: "font-semibold text-P4" })}
+                onClick={(event) => {
+                  handleScroll(event, "Get-App");
+                }}
+              >
                 Get App
               </Link>
             </div>
