45c9e2856b648d38fb51a45d1b9728375c239fd2
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-05-25 23:31:53 +0700

    update - States

--- src/states/index.ts
+++ src/states/index.ts
@@ -2,17 +2,21 @@ import { create } from "zustand";
 
 type States = {
   openAddDataForm?: boolean;
+  openCard?: boolean;
   openUpdateDataForm?: boolean;
 };
 
 type Actions = {
   setOpenAddDataForm: (param: boolean) => void;
+  setOpenCard: (param: boolean) => void;
   setOpenUpdateDataForm: (param: boolean) => void;
 };
 
 export const useGlobalStates = create<Actions & States>((set) => ({
   openAddDataForm: false,
+  openCard: false,
   openUpdateDataForm: false,
   setOpenAddDataForm: (openAddDataForm: boolean) => set({ openAddDataForm }),
+  setOpenCard: (openCard: boolean) => set({ openCard }),
   setOpenUpdateDataForm: (openUpdateDataForm: boolean) => set({ openUpdateDataForm }),
 }));
