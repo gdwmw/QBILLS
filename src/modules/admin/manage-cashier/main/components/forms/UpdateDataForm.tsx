d3ec705b98f96fd2fcd23afa75c77cb829d85497
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-05-25 23:30:42 +0700

    refactor - Menage Cashier

--- src/modules/admin/manage-cashier/main/components/forms/UpdateDataForm.tsx
+++ src/modules/admin/manage-cashier/main/components/forms/UpdateDataForm.tsx
@@ -14,10 +14,10 @@ import { Schema } from "./Schema";
 type TUseForm = Output<typeof Schema>;
 
 type T = {
-  selectedData: ICashierAccount | undefined;
+  data: ICashierAccount | undefined;
 };
 
-const UpdateDataForm: FC<T> = ({ selectedData }): ReactElement => {
+const UpdateDataForm: FC<T> = ({ data }): ReactElement => {
   const queryClient = useQueryClient();
   const { setOpenUpdateDataForm } = useGlobalStates();
   const [loading, setLoading] = useState<boolean>(false);
@@ -28,13 +28,7 @@ const UpdateDataForm: FC<T> = ({ selectedData }): ReactElement => {
     register,
     reset,
   } = useForm<TUseForm>({
-    defaultValues: {
-      id: selectedData?.id,
-      name: selectedData?.name,
-      password: selectedData?.password,
-      role: selectedData?.role,
-      username: selectedData?.username,
-    },
+    defaultValues: { ...data },
     resolver: valibotResolver(Schema),
   });
 
