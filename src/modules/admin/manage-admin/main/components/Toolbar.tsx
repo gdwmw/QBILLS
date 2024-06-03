3c8deccf310abf6db0de09e2b1ccb75363ef6a4e
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-06-03 22:01:21 +0700

    refactor: Update Toolbar component to include search functionality

--- src/modules/admin/manage-admin/main/components/Toolbar.tsx
+++ src/modules/admin/manage-admin/main/components/Toolbar.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactElement } from "react";
+import { FC, FormEventHandler, ReactElement } from "react";
 
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import Image from "next/image";
@@ -11,13 +11,16 @@ import { DELETEMultipleAdminAccount } from "@/utils";
 
 type T = {
   checkbox: string[];
+  handleSubmit: (e: any) => FormEventHandler<HTMLFormElement>;
+  iconOnClick: () => void;
   loading: boolean;
+  onSubmit: (e: any) => void;
   register: any;
   setCheckbox: (value: string[]) => void;
   setLoading: (value: boolean) => void;
 };
 
-export const Toolbar: FC<T> = ({ checkbox, loading, register, setCheckbox, setLoading }): ReactElement => {
+export const Toolbar: FC<T> = ({ checkbox, handleSubmit, iconOnClick, loading, onSubmit, register, setCheckbox, setLoading }): ReactElement => {
   const queryClient = useQueryClient();
   const { setOpenAddDataForm } = useGlobalStates();
 
@@ -40,9 +43,10 @@ export const Toolbar: FC<T> = ({ checkbox, loading, register, setCheckbox, setLo
     <section>
       <div className="flex items-center gap-3 py-5">
         <h2 className="hidden whitespace-nowrap text-xl font-semibold md:block">Admin Account List</h2>
-        <div className="-mt-1.5 ml-auto w-full md:max-w-[350px]">
-          <Input label="Search Account" type="text" {...register("search")} icon={<FaSearch />} />
-        </div>
+        <form className="-mt-1.5 ml-auto w-full md:max-w-[350px]" onSubmit={handleSubmit(onSubmit)}>
+          <Input label="Search Account" type="text" {...register("search")} icon={<FaSearch />} iconOnClick={iconOnClick} />
+          <button className="hidden" type="submit" />
+        </form>
 
         <Button
           className={`min-w-[120px] whitespace-nowrap font-semibold ${loading ? "cursor-wait" : ""}`}
