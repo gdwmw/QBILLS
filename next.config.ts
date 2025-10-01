2d7b47d8d1f75a169986be03b89cc7fcf609d997
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2025-10-01 23:00:35 +0700

    chore: Config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev"],
+  images: {
+    remotePatterns: [
+      {
+        hostname: "68c6ac9c442c663bd027a8ce.mockapi.io",
+        pathname: "/**",
+        port: "",
+        protocol: "https",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
